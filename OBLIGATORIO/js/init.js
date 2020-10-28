8febaf02eb29b31150440a5eedc0cf8aa8f02047
agustinifd <68905493+agustinifd@users.noreply.github.com>
2020-10-27 22:03:30 -0300

    27/10/20
    ARREGLAR NOMBRES DE CARPETAS

--- OBLIGATORIO/js/init.js
+++ OBLIGATORIO/js/init.js
@@ -1,260 +1,295 @@
-document.querySelector("#btnAcceso").addEventListener("click",ingreso);
-document.querySelector("#sellogin").addEventListener("change", ingreso); // Cuando el select de tipo de usuario cambia ejecuta ingreso()
+document.querySelector("#btnAcceso").addEventListener("click", ingreso);
+document.querySelector("#selLogin").addEventListener("change", ingreso); // Cuando el select de tipo de usuario cambia ejecuta ingreso()
 ingreso();
-function ingreso()
-{
-let tipoUsuario = document.querySelector("#sellogin").value;  // Toma el valor del select tipo de usuario y lo guarda en la variable tipoUsuario.
+function ingreso() {
+    let tipoUsuario = document.querySelector("#selLogin").value;  // Toma el valor del select tipo de usuario y lo guarda en la variable tipoUsuario.
 
-mostrarInmueblesListadosYCalificaciones(tipoUsuario); // ESQUELETO HECHO
-mostrarInmueblesListadosVerMas(tipoUsuario); // SIN HACER
-mostrarInmueblesPorPropiedad(tipoUsuario); // SIN HACER
-mostrarHabilitarInmuebles(tipoUsuario); // SIN HACER
-//mostrarRegistroInmubles(tipoUsuario); // SIN HACER
-mostrarOrdenarInmuebles(tipoUsuario);// ESQUELETO HECHO
-mostrarReporteInmuebles(tipoUsuario);//SIN HACER
+    mostrarInmueblesListadosYCalificaciones(tipoUsuario); // 
+    mostrarInmueblesListadosVerMas(tipoUsuario); // 
+    mostrarInmueblesPorPropiedad(tipoUsuario); // 
+    mostrarHabilitarInmuebles(tipoUsuario); // 
+    mostrarRegistroInmubles(tipoUsuario); // 
+    mostrarOrdenarInmuebles(tipoUsuario);//  
+    mostrarReporteInmuebles(tipoUsuario);// 
+    mostrarAcceso(tipoUsuario); //  
+    mostrarRegistro(tipoUsuario);//  
+    mostrarSeleccionarMoneda(tipoUsuario);//  
+    mostrarCotizacion(tipoUsuario);//  
+    mostrarDarAltaAnfitrion(tipoUsuario);//  
+    //mostrarCerrarSesion(tipoUsuario);// 
+    mostrarRealizarReserva(tipoUsuario);//  
+    //mostrarConsultarReserva(tipoUsuario);//  
+    mostrarRelizarCalificacion(tipoUsuario);// 
 
-mostrarAcceso(tipoUsuario); // ESQUELETO HECHO
-mostrarRegistro(tipoUsuario);// ESQUELETO HECHO
-mostrarSeleccionarMoneda(tipoUsuario);// ESQUELETEO HECHO
-mostrarCotizacion(tipoUsuario);// ESQUELETO HECHO
-/*mostrarDarAltaAnfitrion(tipoUsuario);// SIN HACER
-mostrarCerrarSesion(tipoUsuario);// 
-
-mostrarRealizarReserva(tipoUsuario);// SIN HACER
-mostrarConsultarReserva(tipoUsuario);// SIN HACER
-mostrarRelizarCalificacion(tipoUsuario);// SIN HACER
-*/
 }
 function mostrarAcceso(user) {
 
- if (user=="Visitante") 
- {
- document.querySelector("#btnSalir").style.display = "none";
- }  
- if (user=="Huesped") 
- {
-    document.querySelector("#btnSalir").style.display = "inline"; 
- }  
- if (user=="Anfitrion") 
- {
-    document.querySelector("#btnSalir").style.display = "inline"; 
- }  
- if (user=="Administrador") 
- {
-    document.querySelector("#btnSalir").style.display = "inline"; 
- }
-}
-function mostrarRegistro(user)
-{
-    if (user=="Visitante") 
-    {
-        document.querySelector("#divNOREG").style.display = "block";
-        document.querySelector("#divREG").style.display = "block";
-    }  
-    if (user=="Huesped") 
-    {
-        document.querySelector("#divNOREG").style.display = "none";
-        document.querySelector("#divREG").style.display = "none";
-    }  
-    if (user=="Anfitrion") 
-    {
-        document.querySelector("#divNOREG").style.display = "none";
-        document.querySelector("#divREG").style.display = "none";
-    }  
-    if (user=="Administrador") 
-    {
-        document.querySelector("#divNOREG").style.display = "none";
-        document.querySelector("#divREG").style.display = "none";
+    if (user == "Visitante") {
+        document.querySelector("#btnSalir").style.display = "none";
+    }
+    if (user == "Huesped") {
+        document.querySelector("#btnSalir").style.display = "inline";
+    }
+    if (user == "Anfitrion") {
+        document.querySelector("#btnSalir").style.display = "inline";
+    }
+    if (user == "Administrador") {
+        document.querySelector("#btnSalir").style.display = "inline";
     }
 }
-function mostrarSeleccionarMoneda(user)
-{
-    document.querySelector("#selCotiazacion").selectedIndex = 0; //SETEA LA COTIZACION EN PESOS PORQUE SI VIENE SETEADA DESDE DOLARES PUEDE QUE OTRO USUARIO VEA EL VALOR EN DOLARES 
-    if (user=="Visitante") 
-    {
-        document.querySelector("#selCotiazacion").style.display = "inline";
-        document.querySelector("#selCotiazacion").options[1].style.display = "";
-    }  
-    if (user=="Huesped") 
-    {
-        document.querySelector("#selCotiazacion").style.display = "inline";
-        document.querySelector("#selCotiazacion").options[1].style.display = "";
-    }  
-    if (user=="Anfitrion") 
-    {
-        document.querySelector("#selCotiazacion").style.display = "inline";
-        document.querySelector("#selCotiazacion").options[1].style.display = "none"
-    }  
-    if (user=="Administrador") 
-    {
-        document.querySelector("#selCotiazacion").style.display = "none";
-        document.querySelector("#selCotiazacion").options[1].style.display = "none"
+function mostrarRegistro(user) {
+    if (user == "Visitante") {
+        document.querySelector("#divNoReg").style.display = "block";
+        document.querySelector("#divReg").style.display = "block";
     }
-}  
-function mostrarCotizacion(user)
-{
-    if (user=="Visitante") 
-    {
+    if (user == "Huesped") {
+        document.querySelector("#divNoReg").style.display = "none";
+        document.querySelector("#divReg").style.display = "none";
+    }
+    if (user == "Anfitrion") {
+        document.querySelector("#divNoReg").style.display = "none";
+        document.querySelector("#divReg").style.display = "none";
+    }
+    if (user == "Administrador") {
+        document.querySelector("#divNoReg").style.display = "none";
+        document.querySelector("#divReg").style.display = "none";
+    }
+}
+function mostrarSeleccionarMoneda(user) {
+    document.querySelector("#selCotizacion").selectedIndex = 0; //SETEA LA COTIZACION EN PESOS PORQUE SI VIENE SETEADA DESDE DOLARES PUEDE QUE OTRO USUARIO VEA EL VALOR EN DOLARES 
+    if (user == "Visitante") {
+        document.querySelector("#selCotizacion").style.display = "inline";
+        document.querySelector("#selCotizacion").options[1].style.display = "";
+    }
+    if (user == "Huesped") {
+        document.querySelector("#selCotizacion").style.display = "inline";
+        document.querySelector("#selCotizacion").options[1].style.display = "";
+    }
+    if (user == "Anfitrion") {
+        document.querySelector("#selCotizacion").style.display = "inline";
+        document.querySelector("#selCotizacion").options[1].style.display = "none"
+    }
+    if (user == "Administrador") {
+        document.querySelector("#selCotizacion").style.display = "none";
+        document.querySelector("#selCotizacion").options[1].style.display = "none"
+    }
+}
+function mostrarCotizacion(user) {
+    if (user == "Visitante") {
         document.querySelector("#divCotizar").style.display = "none";
-        
-    }  
-    if (user=="Huesped") 
-    {
+
+    }
+    if (user == "Huesped") {
         document.querySelector("#divCotizar").style.display = "none";
-    }  
-    if (user=="Anfitrion") 
-    {
+    }
+    if (user == "Anfitrion") {
         document.querySelector("#divCotizar").style.display = "none";
-    }  
-    if (user=="Administrador") 
-    {
+    }
+    if (user == "Administrador") {
         document.querySelector("#divCotizar").style.display = "inline";
     }
-}  
-function mostrarReporteInmuebles(user)
-{
-    if (user=="Visitante") 
-    {
+}
+function mostrarReporteInmuebles(user) {
+    if (user == "Visitante") {
         document.querySelector("#divReporteInmueble").style.display = "none";
-        
-    }  
-    if (user=="Huesped") 
-    {
+
+    }
+    if (user == "Huesped") {
         document.querySelector("#divReporteInmueble").style.display = "none";
-    }  
-    if (user=="Anfitrion") 
-    {
+    }
+    if (user == "Anfitrion") {
         document.querySelector("#divReporteInmueble").style.display = "none";
-    }  
-    if (user=="Administrador") 
-    {
+    }
+    if (user == "Administrador") {
         document.querySelector("#divReporteInmueble").style.display = "inline";
     }
 
 }
-function mostrarInmueblesListadosYCalificaciones(user)
-{
-    if (user==="Visitante") 
-    {
+function mostrarInmueblesListadosYCalificaciones(user) {
+    if (user === "Visitante") {
         document.querySelector("#divInmuebles").innerHTML = ` <table border=1>
         <tr>
-          <th>Precio propiedad</th>
-          <th>$150</th>
-        </tr>
-        <tr>
-          <td>Calificacion</td>
-          <td>5</td>
+      
+          <th>Título 1</th>
           
         </tr>
-      </table>
-      <img src="img/casa1.jpg">`;
-
+        <tr>
+        <th>Descripción</th>
+        <th>hermosa casa 1</th>
+        </tr>
+        <tr>
+        <th>Ciudad</th>
+        <th>Montevideo</th>
+        </tr>
+        <tr>
         
-    }  
-    if (user==="Huesped") 
-    {
-        document.querySelector("#divInmuebles").innerHTML =         document.querySelector("#divInmuebles").innerHTML = ` <table border=1>
-        <tr>
-          <th>Precio propiedad</th>
+          <th>Precio por Noche</th>
           <th>$150</th>
         </tr>
         <tr>
-          <td>Calificacion</td>
-          <td>5</td>
+          <th>Promedio de Calificacion</th>
+          <th>5</th>
           
         </tr>
       </table>
-      <img src="img/casa1.jpg">`;
-    }  
-    if (user==="Anfitrion") 
-    {
-        document.querySelector("#divInmuebles").innerHTML = "ACA SE MUESTRAN SOLOS LAS CASAS DE SU PROPIEDAD";
-    }  
-    if (user==="Administrador") 
-    {
+      <img id="imagenActual" src="img/casa1.jpg">`;
+
+
+    }
+    if (user === "Huesped") {
+        document.querySelector("#divInmuebles").innerHTML = ` <table border=1>
+        <tr>
+      
+          <th>Título 1</th>
+          
+        </tr>
+        <tr>
+        <th>Descripción</th>
+        <th>hermosa casa 1</th>
+        </tr>
+        <tr>
+        <th>Ciudad</th>
+        <th>Montevideo</th>
+        </tr>
+        <tr>
+        
+          <th>Precio por Noche</th>
+          <th>$150</th>
+        </tr>
+        <tr>
+          <th>Promedio de Calificacion</th>
+          <th>5</th>
+          
+        </tr>
+      </table>
+      <img id="imagenActual" src="img/casa1.jpg">`;
+    }
+    if (user === "Anfitrion") {
+        document.querySelector("#divInmuebles").innerHTML = "ACÁ SE MUESTRAN SOLOS LAS CASAS DE SU PROPIEDAD";
+    }
+    if (user === "Administrador") {
         document.querySelector("#divInmuebles").innerHTML = "";
     }
 }
-function mostrarOrdenarInmuebles(user)
-{
-    if (user=="Visitante") 
-    {
+function mostrarOrdenarInmuebles(user) {
+    if (user == "Visitante") {
         document.querySelector("#selOrdenar").style.display = "inline";
         document.querySelector("#txtBusqueda").style.display = "inline";
-    }  
-    if (user=="Huesped") 
-    {
+    }
+    if (user == "Huesped") {
         document.querySelector("#selOrdenar").style.display = "inline";
         document.querySelector("#txtBusqueda").style.display = "inline";
-    }  
-    if (user=="Anfitrion") 
-    {
+    }
+    if (user == "Anfitrion") {
         document.querySelector("#selOrdenar").style.display = "none";
         document.querySelector("#txtBusqueda").style.display = "none";
-    }  
-    if (user=="Administrador") 
-    {
+    }
+    if (user == "Administrador") {
         document.querySelector("#selOrdenar").style.display = "none";
         document.querySelector("#txtBusqueda").style.display = "none";
     }
 }
-function mostrarInmueblesPorPropiedad(user)
-{
-    if (user=="Visitante") 
-    {
+function mostrarInmueblesPorPropiedad(user) {
+    if (user == "Visitante") {
         document.querySelector("#btnVerInmueblesAnfitrion").style.display = "none";
-        
-    }  
-    if (user=="Huesped") 
-    {
+    }
+    if (user == "Huesped") {
         document.querySelector("#btnVerInmueblesAnfitrion").style.display = "none";
-    }  
-    if (user=="Anfitrion") 
-    {
+    }
+    if (user == "Anfitrion") {
         document.querySelector("#btnVerInmueblesAnfitrion").style.display = "inline";
-    }  
-    if (user=="Administrador") 
-    {
+    }
+    if (user == "Administrador") {
         document.querySelector("#btnVerInmueblesAnfitrion").style.display = "none";
     }
 }
-function mostrarInmueblesListadosVerMas(user)
-{
-    if (user=="Visitante") 
-    {
+function mostrarInmueblesListadosVerMas(user) {
+    if (user == "Visitante") {
         document.querySelector("#divVerMas").style.display = "none";
-        
-    }  
-    if (user=="Huesped") 
-    {
+    }
+    if (user == "Huesped") {
         document.querySelector("#divVerMas").style.display = "block";
-    }  
-    if (user=="Anfitrion") 
-    {
-        document.querySelector("#divVerMas").style.display = "none"; // ME QUEDA DUDA DE SINO MUETRA EL VER MAS DE SUS PROPIAS PROPIEDADES
-    }  
-    if (user=="Administrador") 
-    {
+    }
+    if (user == "Anfitrion") {
+        document.querySelector("#divVerMas").style.display = "none"; 
+    }
+    if (user == "Administrador") {
         document.querySelector("#divVerMas").style.display = "none";
     }
 }
-function mostrarHabilitarInmuebles(user)
-{
-    if (user=="Visitante") 
-    {
+function mostrarHabilitarInmuebles(user) {
+    if (user == "Visitante") {
         document.querySelector("#divHabilitarInmueble").style.display = "none";
-        
-    }  
-    if (user=="Huesped") 
-    {
+
+    }
+    if (user == "Huesped") {
         document.querySelector("#divHabilitarInmueble").style.display = "none";
-    }  
-    if (user=="Anfitrion") 
-    {
-        document.querySelector("#divHabilitarInmueble").style.display = "block"; 
-    }  
-    if (user=="Administrador") 
-    {
+    }
+    if (user == "Anfitrion") {
+        document.querySelector("#divHabilitarInmueble").style.display = "block";
+    }
+    if (user == "Administrador") {
         document.querySelector("#divHabilitarInmueble").style.display = "none";
     }
 }
+function mostrarRegistroInmubles(user) {
+    if (user == "Visitante") {
+        document.querySelector("#divRegistrarInmueble").style.display = "none";
+    }
+    if (user == "Huesped") {
+        document.querySelector("#divRegistrarInmueble").style.display = "none";
+    }
+    if (user == "Anfitrion") {
+        document.querySelector("#divRegistrarInmueble").style.display = "inline";
+    }
+    if (user == "Administrador") {
+        document.querySelector("#divRegistrarInmueble").style.display = "none";
+    }
+}
+function mostrarDarAltaAnfitrion(user) {
+    if (user == "Visitante") {
+        document.querySelector("#btnAltaAnfitrion").style.display = "none";
+    }
+    if (user == "Huesped") {
+        document.querySelector("#btnAltaAnfitrion").style.display = "none";
+    }
+    if (user == "Anfitrion") {
+        document.querySelector("#btnAltaAnfitrion").style.display = "none";
+    }
+    if (user == "Administrador") {
+        document.querySelector("#btnAltaAnfitrion").style.display = "block";
+    }
+}
+function mostrarRealizarReserva(user) {
+    if (user == "Visitante") {
+        document.querySelector("#divReserva").style.display = "none";
+    }
+    if (user == "Huesped") {
+        document.querySelector("#divReserva").style.display = "block";
+    }
+    if (user == "Anfitrion") {
+        document.querySelector("#divReserva").style.display = "none";
+    }
+    if (user == "Administrador") {
+        document.querySelector("#divReserva").style.display = "none";
+    }
+}
+
+//function mostrarConsultarReserva(user)
+//{}
+
+function mostrarRelizarCalificacion(user) {
+    if (user == "Visitante") {
+        document.querySelector("#divCalificarReserva").style.display = "none";
+    }
+    if (user == "Huesped") {
+        document.querySelector("#divCalificarReserva").style.display = "block";
+    }
+    if (user == "Anfitrion") {
+        document.querySelector("#divCalificarReserva").style.display = "none";
+    }
+    if (user == "Administrador") {
+        document.querySelector("#divCalificarReserva").style.display = "none";
+    }
+}
+
